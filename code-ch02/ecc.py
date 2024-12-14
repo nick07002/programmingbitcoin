e6ea057763f795383e25d7a1e4057c05bdf6c8da
Nick <nickrede07002@gmail.com>
2024-12-14 10:55:12 -0800

    finish chapter 2

--- code-ch02/ecc.py
+++ code-ch02/ecc.py
@@ -142,7 +142,7 @@ class Point:
 
     def __ne__(self, other):
         # this should be the inverse of the == operator
-        raise NotImplementedError
+        return not (self == other)
 
     def __repr__(self):
         if self.x is None:
@@ -177,7 +177,18 @@ class Point:
         # x3=s**2-2*x1
         # y3=s*(x1-x3)-y1
 
-        raise NotImplementedError
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None,None,self.a, self.b)
+        if self.x != other.x:
+            s = (other.y-self.y)/(other.x-self.x)
+            x3 = s**2 - self.x - other.x
+            y3 = s*(self.x-x3) - self.y
+            return self.__class__(x3,y3,self.a,self.b)
+        if self == other:
+            s = (3*self.x**2 + self.a)/(2*self.y)
+            x3 = s**2 - 2*self.x
+            y3 = s*(self.x-x3)-self.y
+            return self.__class__(x3,y3,self.a,self.b)
 
 
 class PointTest(TestCase):
